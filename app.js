76e8e516d1a23f0d656c088f49d32c681268241f
unknown <pcampbell@HCDEVPC034.Development.HealthyChoices.gsi.gov.uk>
2012-12-21 13:56:49 +0000

    buildin out the process.

--- app.js
+++ app.js
@@ -8,15 +8,38 @@ var start = function(){
 
  console.log('maptwit process started');
  
+var twitter = new Twitter({
+    consumer_key: 'FPRURnejrFbLEpXKxO79TQ',
+    consumer_secret: 'HeNfvVEE9O0DdyNnq5yxX5iutNtufSBHc9gAY3tc15U',
+    access_token_key: '*',
+    access_token_secret: '*'
+    
+});
+
+
+ 
    Models.HashTag.find(function(err, hashtags){
      if(err)
        console.log(err);
      else {
+         var tags = '';
          hashtags.forEach(function(tag){
-            console.log('collecting twitter updates for ' + tag.tag);
-            
-            
+            tags = tags + ',' + tag.tag
          });
+         tags = tags.substring(1, tags.length)
+         console.log('collecting twitter updates for ' + tags);
+         twitter.stream('statuses/filter', {'track':tags}, function(stream) {
+           stream.on('data', function(data) {
+                console.log(data);
+              });
+           stream.on('end', function (response) {
+             console.log('ended');
+             console.log(response)
+           });
+           stream.on('destroy', function (response) {
+             console.log('destroyed');
+           });
+           });
      }
       
    })
